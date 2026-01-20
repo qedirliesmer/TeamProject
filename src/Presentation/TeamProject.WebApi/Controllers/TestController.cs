cf5008b5953b7e0516e72e54e1d515464dc1b186
esmerqedirli <smrqdirli14@gmail.com>
2026-01-20 11:34:05 -0800

    Added TestController

---
+++ src/Presentation/TeamProject.WebApi/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamProject.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("The branch is created");
+    }
+}
